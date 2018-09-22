693d197ef4005fc4e0cd253eadf267a0a2d41629
Brian Landes <the.brian.landes@gmail.com>
2018-09-21 18:09:10 -0600

    Initial Project Setup

---
+++ Assets/_Scripts/CameraFollowObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Causes the main camera to follow the attached gameobject at the given distance and angle
+/// </summary>
+[ExecuteInEditMode]
+public class CameraFollowObject : MonoBehaviour {
+
+    public Vector3 offset = Vector3.up + Vector3.back;
+    
+	void Update () {
+        Camera.main.transform.position = transform.position + offset;
+        Camera.main.transform.LookAt(transform);
+	}
+}
---
+++ Assets/_Scripts/LevelGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Follows the given game object and generates ground objects up to a certain distance away
+/// </summary>
+public class LevelGenerator : MonoBehaviour {
+
+    public GameObject player;
+
+    public float genDistance = 10f;
+
+    public List<GameObject> groundObjects = new List<GameObject>();
+
+    /// <summary>
+    /// The position of the player when we last generated a ground object
+    /// </summary>
+    private Vector3 lastSpawnPosition;
+
+    void Start()
+
+	void Update () {
+		
+	}
+}
