955fd18a515b532a9b34ede36bf22beb2e24c35e
singh1rocks <shakti.das@aol.com>
2018-09-21 19:41:09 -0600

    Basic Player Controller

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 
 public class PlayerController : MonoBehaviour {
+    
+    private Rigidbody playerRB;
 
-    private CharacterController controller;
+    private Vector3 movementVector;
 
-    public float speed = 5.0f;
+    public float forwardSpeed;
+    public float horizontalSpeed;
 
     // Use this for initialization
     void Start () {
-        controller = GetComponent<CharacterController>();
+        playerRB = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        controller.Move((Vector3.forward * speed) * Time.deltaTime);
+	void FixedUpdate () {
+
+        movementVector = Vector3.zero;
+
+        movementVector.x = Input.GetAxisRaw("Horizontal") * horizontalSpeed;
+
+        movementVector.y = Input.GetAxisRaw("Vertical");
+
+        movementVector.z = forwardSpeed;
+
+        playerRB.AddForce(movementVector);
+
 	}
 }
