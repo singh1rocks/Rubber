e2dcaee05d9fb75ad1dad38c88e9cdc306b4598c
ryanduane <ryanaduane@gmail.com>
2018-09-21 22:47:27 -0600

    Shield

---
+++ Assets/_Scripts/Shield.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour {
+    public GameObject powerUp;
+    MeshRenderer powerUpMesh;
+    public GameObject player;
+    PlayerHealth playerHP;
+	// Use this for initialization
+	void Start () {
+        powerUpMesh = powerUp.GetComponent<MeshRenderer>();
+        playerHP = player.GetComponent<PlayerHealth>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerHP.enabled = false;
+            powerUpMesh.enabled = false;
+        }
+        else if(other.gameObject.CompareTag("Nails"))
+        {
+            playerHP.enabled = true;
+            Destroy(gameObject);
+
+        }
+    }
+}
+
