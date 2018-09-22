dadd5a133a239b3e44660efc8b1cd2d1ad7d6e41
Allan Feller <16980156+AFeller9999@users.noreply.github.com>
2018-09-21 21:38:18 -0600

    Modification of Owner actor to snap to the ground below him

--- Assets/_Scripts/OwnerFollow.cs
+++ Assets/_Scripts/OwnerFollow.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class OwnerFollow : MonoBehaviour {
 
-    public GameObject player;
-    float HorizontalSpeed;
+    public GameObject player; // The player object.
+    float HorizontalSpeed; // Should not be touched. Can be read to get the change in the desired X position.
+    private float rayDistance = 100f; // Should be some remarkably high but stable distance.
 
     Animator animator;
 
 
 	void Start () {
-       // player = GameObject.FindGameObjectWithTag("Player"); // The player object
+       // player = GameObject.FindGameObjectWithTag("Player"); // The player object.
         animator = GetComponentInChildren<Animator>();
         animator.Play(0);
     }
 
 	void Update () {
+        float yPos = determineYOffset();
+        Debug.Log(yPos);
         float desiredX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref HorizontalSpeed, .1f);
-        transform.position = new Vector3(desiredX, player.transform.position.y, player.transform.position.z - 3);
+        transform.position = new Vector3(desiredX, yPos, player.transform.position.z - 3);
 	}
+
+    public float determineYOffset()
+    {
+        // Determine the standing position of the actor based on the ground below him.
+        Ray rayDown = new Ray(transform.position - new Vector3(0,-.5f), Vector3.down * rayDistance);
+        //Debug.DrawRay(transform.position - new Vector3(0, -.5f), Vector3.down * rayDistance, Color.red);
+        RaycastHit hit = new RaycastHit();
+        float yOffset = 0f;
+        Physics.Raycast(rayDown, out hit, rayDistance);
+        if (hit.collider == null) return player.transform.position.y;
+        return (hit.point.y + yOffset);
+    }
 }
