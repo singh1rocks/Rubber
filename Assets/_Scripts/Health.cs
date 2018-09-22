c14d4ef14413e0ad2a77655e6a69d67506d95fc8
ryanduane <ryanaduane@gmail.com>
2018-09-21 20:02:07 -0600

    Health Fix

--- Assets/_Scripts/Health.cs
+++ Assets/_Scripts/Health.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collectibles : MonoBehaviour {
+public class Health : MonoBehaviour {
 
-    private void OnTriggerEnter(Collider other)
+  /*  private void OnTriggerEnter(Collider other)
     {
         other.health += 1;
         Destroy(this);
    
-    }
+    } */
 }
