27c2e5fb597bd2fe3f91e1d63ac41a9903b3e7e1
ryanduane <ryanaduane@gmail.com>
2018-09-21 19:42:40 -0600

    Collectibles

---
+++ Assets/_Scripts/Coin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+    public Score score;
+
+    private void Start()
+    {
+        GameObject score = GameObject.Find("ScoreObject");
+    }
+    private void OnTriggerEnter(Collider Score)
+    {
+        score += 10;
+        Destroy(this);
+    }
+}
