0ddb3f9142395b7d52a938ebcbbf762690648348
DeltaLogic <bfisher0240@gmail.com>
2018-09-21 19:39:50 -0600

    Fixes score object , and edits score script

--- Assets/_Scripts/Score.cs
+++ Assets/_Scripts/Score.cs
@@ -22,21 +22,22 @@ public class Score : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        //Creates the time to start up
         time += Time.deltaTime * 10;
-        createScore();
-
+        //waits waitTime seconds and increments score by scoreAdd
+        WaitAndAdd(5, 1);
+        
+        //Debugging
         Debug.Log(score);
         Debug.Log(time);
 	}
 
-    public void createScore()
-    {
-        score = time * 2;
-    }
 
-    public void doubleScore()
+    //Waits waitTime seconds and increments score by scoreAdd 
+    IEnumerator WaitAndAdd(float waitTime, int scoreAdd)
     {
+        yield return new WaitForSeconds(waitTime);
+        score = score + scoreAdd;
         
-
     }
 }
